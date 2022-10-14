eff32cddea95476bc8a7a583105d0708de5d9c17
Marko <marko.kuntijarvi@metropolia.fi>
2022-10-14 12:03:53 +0300

    Final Esiprojekti

--- test1.py
+++
@@ -1,9 +0,0 @@
-thisdict = {
-  "brand": ["Ford","Fiat", "Mercedes", "Honda"],
-  "model": "Mustang",
-  "year": 1964
-}
-
-# print(thisdict)
-# print(thisdict["brand"])
-# print(thisdict["brand"][2])
